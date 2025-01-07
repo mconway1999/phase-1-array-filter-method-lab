29fe1d43e6eabe785e45173254201d4a1b18de88
Madeline Conway <conway.maddy@yahoo.com>
2025-01-07 15:22:41 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(drivers, nameToMatch) {
+    return drivers.filter(driver => driver.toLowerCase() === nameToMatch.toLowerCase());
+}
+
+function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.startsWith(query));
+  }
+
+function matchName(drivers, nameToMatch){
+    return drivers.filter(driver => driver.name === nameToMatch)
+}
